3784744253e26fd83106c21d5f2d91e3867c4f39
rmatsunaga <matsunagar97@gmail.com>
2018-07-21 15:53:33 -0700

    Initialized questions and answers

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Answer extends React.Component{
-  render(){
+
+
+function Answer(props) {
+  const answer = [
+    [2009, 2012, 2018, 2017],
+    ["Bboy Samson", "Bboy Pele", "Bgirl Selma Soul", "Bboy Kulani"],
+    ["Joe Stolte", "Alien Ness", "Keebz", "Andre Nguyen"],
+    ["Uncomfortably Fresh 17th Ann", "Break Wars", "Pro Kids 10th Ann", "Daft Funk Crew: Manko's Birthday"]
+  ];
+
     return(
       <div>
-        <input type="checkbox" />
-        <div>2009</div>
+        <div><input type="checkbox" /> {answer[props.aNum][0]}</div>
+        <div><input type="checkbox" /> {answer[props.aNum][1]}</div>
+        <div><input type="checkbox" /> {answer[props.aNum][2]}</div>
+        <div><input type="checkbox" /> {answer[props.aNum][3]}</div>
       </div>
-    );
-  }
+  );
 }
 
 function Question (props){
 
-  var question = {
-    0: "What year did Dialtone & Fever One battle into Massive Monkees?",
-    1: "Who won 2nd at the Northwest Sweet 16 in 2018?",
-    2: "In 2018 at Massive Monkee Day Footwork battle tiebreaker for the top 8 position, who notified Bboy Makoto that he made it?",
-    3: "What jam did Deathray make an appearance with a Canadiam bboy in 2018?"
-  };
+  var question = [
+    "What year did Dialtone & Fever One battle into Massive Monkees?",
+    "Who won 2nd at the Northwest Sweet 16 in 2018?",
+    "In 2018 at Massive Monkee Day Footwork battle tiebreaker for the top 8 position, who notified Bboy Makoto that he made it?",
+    "What jam did Deathray make an appearance with a Canadiam bboy in 2018?"
+  ];
   return(
     <h2>
-      {question.props.qNum}
+      {question[props.qNum]}
     </ h2>
   );
 
@@ -35,13 +44,14 @@ class Questions extends React.Component{
     return(
       <div>
         <Question qNum = {this.props.qNum} />
-        <Answer qNum = {this.props.qNum} />
+        <Answer aNum = {this.props.qNum} />
       </div>
     );
   }
 }
 
 class Quiz extends React.Component {
+
   render(){
     return(
       <div>
